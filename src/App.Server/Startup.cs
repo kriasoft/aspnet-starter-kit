5a492d365ca9ca6e00acb7bedd1f798340fe4ad1
Konstantin Tarkus <hello@tarkus.me>
2014-08-21 13:49:41 +0400

    Initial commit

---
+++ src/App.Server/Startup.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) KriaSoft, LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(App.Server.Startup))]
+
+namespace App.Server
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
---
+++ test/App.Server.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Server.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
