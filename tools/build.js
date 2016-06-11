9cff388c7365e9c23a7f9919a4bc252f742e6553
Konstantin Tarkus <hello@tarkus.me>
2016-06-11 23:20:09 +0300

    Create appsettings.<env>.json files on build if they don't exist

--- tools/build.js
+++ tools/build.js
@@ -7,6 +7,7 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
+const fs = require('fs');
 const path = require('path');
 const cp = require('./lib/cp');
 const task = require('./lib/task');
@@ -15,8 +16,28 @@ const isDebug = !(process.argv.includes('--production') || process.argv.includes
 const config = isDebug ? 'Debug' : 'Release';
 
 module.exports = task('build', () => Promise.resolve()
+  // Clean up the output directory
   .then(() => require('./clean'))
+
+  // Create Production/Development configuration files if they don't exist
+  .then(() => new Promise(resolve => {
+    let count = 2;
+    const appSettings = require('../server/appsettings.json'); // use it as a template
+    delete appSettings.Logging;
+    ['Production', 'Development'].forEach(env => {
+      const filename = path.resolve(__dirname, `../server/appsettings.${env}.json`);
+      fs.writeFile(filename, JSON.stringify(appSettings, null, '  '), { flag: 'wx', }, err => {
+        if (--count === 0) resolve();
+      });
+    })
+  }))
+
+  // Compile the ASP.NET Core app
   .then(() => cp.spawn('dotnet', ['publish', 'server', '-o', 'build', '-c', config, '-r', 'coreclr']))
+
+  // Compile the JavaScript app
   .then(() => require('./bundle'))
+
+  // Copy static files to the output folder
   .then(() => require('./copy'))
 );
