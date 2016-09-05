3f9abff4e8e6fca6757b35a13657da244cedbfec
Konstantin Tarkus <hello@tarkus.me>
2016-09-05 09:41:11 +0300

    Update the error page

--- client/views/error/ErrorPage.js
+++ client/views/error/ErrorPage.js
@@ -8,25 +8,50 @@
  */
 
 import React from 'react';
-import Layout from '../../components/Layout';
+import history from '../../history';
+import Link from '../../components/Link';
+import s from './ErrorPage.css';
 
 class ErrorPage extends React.Component {
 
+  static propTypes = {
+    error: React.PropTypes.object,
+  };
+
   componentDidMount() {
-    document.title = 'Page Not Found';
+    document.title = this.props.error && this.props.error.status === 404 ?
+      'Page Not Found' : 'Error';
   }
+
+  goBack = event => {
+    event.preventDefault();
+    history.goBack();
+  };
 
   render() {
+    // eslint-disable-next-line no-console
+    if (this.props.error && console.error) console.error(this.props.error);
+
+    const [code, title] = this.props.error && this.props.error.status === 404 ?
+      ['404', 'Page not found'] :
+      ['Error', 'Oups, something went wrong'];
+
     return (
-      <Layout>
-        <h1 className="mdl-typography--title">Page Not Found</h1>
-        <p className="mdl-typography--body-1">
-          Sorry, this page does not exist.
-        </p>
-      </Layout>
+      <div className={s.container}>
+        <main className={s.content}>
+          <h1 className={s.code}>{code}</h1>
+          <p className={s.title}>{title}</p>
+          {code === '404' && <p className={s.text}>
+            The page you're looking for does not exist or an another error occurred.
+          </p>}
+          <p className={s.text}>
+            <a href="/" onClick={this.goBack}>Go back</a>, or head over to the&nbsp;
+            <Link to="/">home page</Link> to choose a new direction.
+          </p>
+        </main>
+      </div>
     );
   }
-
 }
 
 export default ErrorPage;
