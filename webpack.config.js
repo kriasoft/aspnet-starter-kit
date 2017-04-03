231ffa3b5aac46a6b15bdc59427e235856b959e6
George <george.saadeh@tigerspike.com>
2017-04-03 17:54:46 +0400

    Upgraded .NET core to 1.1, upgraded packages including react hot loader to work with the latest version of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ const config = {
   // The entry point for the bundle
   entry: [
     /* Material Design Lite (https://getmdl.io) */
-    '!!style!css!react-mdl/extra/material.min.css',
+    '!!style-loader!css-loader!react-mdl/extra/material.min.css',
     'react-mdl/extra/material.min.js',
     /* The main entry point of your JavaScript application */
     './main.js',
@@ -46,9 +46,6 @@ const config = {
     chunkFilename: isDebug ? '[id].js?[chunkhash]' : '[id].[chunkhash].js',
     sourcePrefix: '  ',
   },
-
-  // Switch loaders to debug or release mode
-  debug: isDebug,
 
   // Developer tool to enhance debugging, source maps
   // http://webpack.github.io/docs/configuration.html#devtool
@@ -80,6 +77,53 @@ const config = {
       path: path.resolve(__dirname, './public/dist'),
       filename: 'assets.json',
       prettyPrint: true,
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        // The list of plugins for PostCSS
+        // https://github.com/postcss/postcss
+        postcss: [
+          // Transfer @import rule by inlining content, e.g. @import 'normalize.css'
+          // https://github.com/postcss/postcss-import
+          // require('postcss-import')({ addDependencyTo: bundler }),
+          // W3C variables, e.g. :root { --color: red; } div { background: var(--color); }
+          // https://github.com/postcss/postcss-custom-properties
+          require('postcss-custom-properties')(),
+          // W3C CSS Custom Media Queries, e.g. @custom-media --small-viewport (max-width: 30em);
+          // https://github.com/postcss/postcss-custom-media
+          require('postcss-custom-media')(),
+          // CSS4 Media Queries, e.g. @media screen and (width >= 500px) and (width <= 1200px) { }
+          // https://github.com/postcss/postcss-media-minmax
+          require('postcss-media-minmax')(),
+          // W3C CSS Custom Selectors, e.g. @custom-selector :--heading h1, h2, h3, h4, h5, h6;
+          // https://github.com/postcss/postcss-custom-selectors
+          require('postcss-custom-selectors')(),
+          // W3C calc() function, e.g. div { height: calc(100px - 2em); }
+          // https://github.com/postcss/postcss-calc
+          require('postcss-calc')(),
+          // Allows you to nest one style rule inside another
+          // https://github.com/jonathantneal/postcss-nesting
+          require('postcss-nesting')(),
+          // W3C color() function, e.g. div { background: color(red alpha(90%)); }
+          // https://github.com/postcss/postcss-color-function
+          require('postcss-color-function')(),
+          // Convert CSS shorthand filters to SVG equivalent, e.g. .blur { filter: blur(4px); }
+          // https://github.com/iamvdo/pleeease-filters
+          require('pleeease-filters')(),
+          // Generate pixel fallback for "rem" units, e.g. div { margin: 2.5rem 2px 3em 100%; }
+          // https://github.com/robwierzbowski/node-pixrem
+          require('pixrem')(),
+          // W3C CSS Level4 :matches() pseudo class, e.g. p:matches(:first-child, .special) { }
+          // https://github.com/postcss/postcss-selector-matches
+          require('postcss-selector-matches')(),
+          // Transforms :not() W3C CSS Level 4 pseudo class to :not() CSS Level 3 selectors
+          // https://github.com/postcss/postcss-selector-not
+          require('postcss-selector-not')(),
+          // Add vendor prefixes to CSS rules using values from caniuse.com
+          // https://github.com/postcss/autoprefixer
+          require('autoprefixer')(),
+        ],
+      },
     }),
   ],
 
@@ -139,64 +183,23 @@ const config = {
       },
     ],
   },
-
-  // The list of plugins for PostCSS
-  // https://github.com/postcss/postcss
-  postcss() {
-    return [
-      // Transfer @import rule by inlining content, e.g. @import 'normalize.css'
-      // https://github.com/postcss/postcss-import
-      // require('postcss-import')({ addDependencyTo: bundler }),
-      // W3C variables, e.g. :root { --color: red; } div { background: var(--color); }
-      // https://github.com/postcss/postcss-custom-properties
-      require('postcss-custom-properties')(),
-      // W3C CSS Custom Media Queries, e.g. @custom-media --small-viewport (max-width: 30em);
-      // https://github.com/postcss/postcss-custom-media
-      require('postcss-custom-media')(),
-      // CSS4 Media Queries, e.g. @media screen and (width >= 500px) and (width <= 1200px) { }
-      // https://github.com/postcss/postcss-media-minmax
-      require('postcss-media-minmax')(),
-      // W3C CSS Custom Selectors, e.g. @custom-selector :--heading h1, h2, h3, h4, h5, h6;
-      // https://github.com/postcss/postcss-custom-selectors
-      require('postcss-custom-selectors')(),
-      // W3C calc() function, e.g. div { height: calc(100px - 2em); }
-      // https://github.com/postcss/postcss-calc
-      require('postcss-calc')(),
-      // Allows you to nest one style rule inside another
-      // https://github.com/jonathantneal/postcss-nesting
-      require('postcss-nesting')(),
-      // W3C color() function, e.g. div { background: color(red alpha(90%)); }
-      // https://github.com/postcss/postcss-color-function
-      require('postcss-color-function')(),
-      // Convert CSS shorthand filters to SVG equivalent, e.g. .blur { filter: blur(4px); }
-      // https://github.com/iamvdo/pleeease-filters
-      require('pleeease-filters')(),
-      // Generate pixel fallback for "rem" units, e.g. div { margin: 2.5rem 2px 3em 100%; }
-      // https://github.com/robwierzbowski/node-pixrem
-      require('pixrem')(),
-      // W3C CSS Level4 :matches() pseudo class, e.g. p:matches(:first-child, .special) { }
-      // https://github.com/postcss/postcss-selector-matches
-      require('postcss-selector-matches')(),
-      // Transforms :not() W3C CSS Level 4 pseudo class to :not() CSS Level 3 selectors
-      // https://github.com/postcss/postcss-selector-not
-      require('postcss-selector-not')(),
-      // Add vendor prefixes to CSS rules using values from caniuse.com
-      // https://github.com/postcss/autoprefixer
-      require('autoprefixer')(),
-    ];
-  },
-
 };
 
 // Integrate Webpack 2.x (disable ES2015 modules)
 babelConfig.presets[babelConfig.presets.indexOf('latest')] = ['latest', {
-  es2015: { modules: false },
+  es2015: {
+    modules: false,
+  },
 }];
 
 // Optimize the bundle in release (production) mode
 if (!isDebug) {
   config.plugins.push(new webpack.optimize.DedupePlugin());
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: isVerbose } }));
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: isVerbose,
+    },
+  }));
   config.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
 }
 
@@ -205,7 +208,7 @@ if (isDebug && useHMR) {
   babelConfig.plugins.unshift('react-hot-loader/babel');
   config.entry.unshift('react-hot-loader/patch', 'webpack-hot-middleware/client');
   config.plugins.push(new webpack.HotModuleReplacementPlugin());
-  config.plugins.push(new webpack.NoErrorsPlugin());
+  config.plugins.push(new webpack.NoEmitOnErrorsPlugin());
 }
 
 module.exports = config;
