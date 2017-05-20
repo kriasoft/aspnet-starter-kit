505ba3aab28a11d5fdf2a907dfd5fe9dba7c1bec
Fredrik Mårlind <fmarlind@gmail.com>
2017-05-20 21:55:46 +0200

    This commit fixes issue #32 (#35)

--- run.js
+++ run.js
@@ -91,7 +91,7 @@ tasks.set('build', () => {
     .then(() => new Promise((resolve, reject) => {
       const options = { stdio: ['ignore', 'inherit', 'inherit'] };
       const config = global.DEBUG ? 'Debug' : 'Release';
-      const args = ['publish', 'server', '-o', 'build', '-c', config, '-r', 'coreclr'];
+      const args = ['publish', 'server', '-o', '../build', '-c', config];
       cp.spawn('dotnet', args, options).on('close', code => {
         if (code === 0) {
           resolve();
