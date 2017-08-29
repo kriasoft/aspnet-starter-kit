24b43be02bf8b2884917a9d2c1353782b680dfbb
Joshua Adams <n7down@gmail.com>
2017-08-29 15:54:19 -0600

    updated to dotnet 2.0

--- server/Models/User.cs
+++ server/Models/User.cs
@@ -2,7 +2,7 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE.txt file in the root directory of this source tree.
 
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace Server.Models
 {
